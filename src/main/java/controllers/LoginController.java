651616c05c5bd4992bb925c56818047aa8af70a6
evieellis <evie.ellis11@gmailc.om>
2023-12-04 17:48:53 -0500

    little changes

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane email;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button signUp;
+
+}
