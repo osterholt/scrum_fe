93ac48896e3b41713d938ceef3cb6f5b1365593c
Cam Osterholt <github@osterholt.us>
2023-12-04 17:40:35 -0500

    update messed up merge

--- src/main/java/controllers/BoardController.java
+++ src/main/java/controllers/BoardController.java
@@ -25,21 +25,27 @@ public class BoardController implements Initializable {
     @FXML
     private Text welcomeText;
 
+    @FXML
+    private Label welcomeLabel;
+
     private boolean setWelcomeText(String firstName, String lastName) {
         if(firstName != null && lastName != null) {
             welcomeText.setText("Welcome: " + firstName + " " + lastName);
+            welcomeLabel.setText("Welcome: " + firstName + " " + lastName);
         }
         return firstName != null && lastName != null;
     }
 
     @FXML
     void displayBoards(MouseEvent event) {
-        
+        companyChoices.show();
+        System.out.println("Display Boards Pressed");
+        // System.out.println(companyChoices);
     }
 
     @FXML
     void displayCompanies(ContextMenuEvent event) {
-        
+        System.out.println("Display Companies Pressed");
     }
 
     @Override
@@ -48,18 +54,27 @@ public class BoardController implements Initializable {
         //FIXME: THis is all in the wrong method.
         User activeUser = AppFacade.getInstance().getActiveUser();
         setWelcomeText(activeUser.getFirstName(), activeUser.getLastName());
-        // double x = welcomeText.getX();
-        // double y = welcomeText.getY();
-        // companyChoices.setLayoutX(x + DROPDOWN_X_OFFSET);
-        // companyChoices.setLayoutY(y);
+        setDropdown();
+    }
+    private void setDropdown() {
+        double x = welcomeLabel.getLayoutX();
+        x += (welcomeLabel.getWidth() + DROPDOWN_X_OFFSET); //FIXME: width is set to default 0??
+        double y = welcomeLabel.getLayoutY();
+        y += (welcomeLabel.getHeight() / 2);
 
-        // // Add initial items to the ChoiceBox
-        // ObservableList<String> items = FXCollections.observableArrayList();
-        // ArrayList<Company> companies = AppFacade.getInstance().getActiveUser().getCompanies();
-        // for(Company company : companies) {
-        //     items.add(company.getName());
-        // }
-        // companyChoices.setItems(items);
+        System.out.println("DEBUG: companyChoices set location: (" + x + ", " + y + ")");
+        companyChoices.setLayoutX(x);
+        companyChoices.setLayoutY(y);
+        System.out.println("DEBUG: companyChoices measured location: (" + companyChoices.getLayoutX() + ", " + companyChoices.getLayoutY() + ")");
+
+        // Add initial items to the ChoiceBox
+        ObservableList<String> items = FXCollections.observableArrayList();
+        ArrayList<Company> companies = AppFacade.getInstance().getActiveUser().getCompanies();
+        for(Company company : companies) {
+            items.add(company.getName());
+        }
+        items.add("DEBUG: DEFAULT");
+        companyChoices.setItems(items);
     }
 
 }
