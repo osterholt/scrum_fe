b25a66576b8f052f8043f27c87638bbac529a996
evieellis <evie.ellis11@gmailc.om>
2023-12-05 09:28:41 -0500

    back buttons yay!

--- src/main/java/controllers/SignUpController.java
+++ src/main/java/controllers/SignUpController.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class SignUpController {
@@ -27,6 +28,23 @@ public class SignUpController {
     private Button signUp;
 
     @FXML
+    private Button backToWelcome;
+
+    @FXML
+    private Label errorLabelInvalid; 
+
+    @FXML
+    private Label errorLabelAlreadyExists;
+
+    @FXML
+    void backToWelcome(ActionEvent event) throws IOException {
+        errorLabelInvalid.setVisible(false);
+        errorLabelAlreadyExists.setVisible(false);
+        System.out.println("DEBUG: Back button pressed!");
+        App.setRoot("welcome");
+    }
+
+    @FXML
     void signUp(ActionEvent event) throws IOException {
         String[] fullNameArr = fullName.getText().split(SPACE);
         String emailStr = email.getText();
@@ -37,19 +55,27 @@ public class SignUpController {
             User user = LoginManager.getInstance().getUser(userID);
             DataWriter.saveUsers();
             //Go to Board Display
-            if(user != null) {
-                System.out.println("DEBUG: Sign Up Success!!");
-                System.out.println("Active User: " + AppFacade.getInstance().getActiveUser());
-                // BoardController.setWelcomeText(user.getFirstName(), user.getLastName());
-                //Set Companies!
-                App.setRoot("board");
-            }
-            else {
-                //TODO: Throw error message.
-            }
+            System.out.println("DEBUG: Sign Up Success!!");
+            System.out.println("Active User: " + AppFacade.getInstance().getActiveUser());
+            // BoardController.setWelcomeText(user.getFirstName(), user.getLastName());
+            //Set Companies!
+            App.setRoot("board");
+            
         }
         else {
-            //TODO: Throw error message.
+            if(LoginManager.getInstance().getUser(emailStr) != null) {
+                // User is null, show the error label
+            System.out.println("DEBUG: Sign Up Failed, Already Exists!!");
+            errorLabelInvalid.setVisible(false);
+            errorLabelAlreadyExists.setVisible(true);
+            }
+            else {
+                // User is null, show the error label
+                System.out.println("DEBUG: Sign Up Failed, Invalid!!");
+                errorLabelAlreadyExists.setVisible(false);
+                errorLabelInvalid.setVisible(true);
+
+            }
         }
     }
 
